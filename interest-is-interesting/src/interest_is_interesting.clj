1b265e54048af3949a68fdf5c9ebe2f6c01cbd98
filipebafica <filipebafica@hotmail.com>
2024-11-15 21:51:03 -0300

    interest-is-interesting-exercise 2

--- interest-is-interesting/src/interest_is_interesting.clj
+++ interest-is-interesting/src/interest_is_interesting.clj
@@ -3,21 +3,17 @@
 (defn interest-rate
   "Returns the interest rate based on the specified balance."
   [balance]
-  (cond (neg? balance) -3.213
-        (< balance 1000M) 0.5
-        (< balance 5000M) 1.621
-        :else 2.475
+  (cond 
+    (< balance 0) -3.213
+    (< balance 1000) 0.5
+    (< balance 5000) 1.621
+    :else  2.475
     )
   )
 
 (defn annual-balance-update
   "Returns the annual balance update, taking into account the interest rate."
   [balance]
-  ;; (let [rate (/ (interest-rate balance) 100)
-  ;;       updated-balance (* (+ 1 (bigdec rate)) balance)]
-  ;;   (bigdec updated-balance)
-  ;;   )
-
   (let [rate (bigdec (Math/abs (/ (interest-rate balance) 100)))]
        (* (+ 1M rate) balance)
     )
@@ -26,9 +22,8 @@
 (defn amount-to-donate
   "Returns how much money to donate based on the balance and the tax-free percentage."
   [balance tax-free-percentage]
-  (cond (>= balance 0) (let [rate (/ tax-free-percentage 100)]
-                         (int (* balance rate 2))
-                       )
-        :else 0
+  (cond
+    (> balance 0) (int (* 2 (/ tax-free-percentage 100 ) balance))
+    :else 0
     )
   )
